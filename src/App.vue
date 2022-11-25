350b7861cc5ea9585ff575ca3e597151dccfb206
da-ferreira <ferreiradavid361@gmail.com>
2022-11-24 22:16:01 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+    </v-app-bar>
+
+    <v-main class="ma-3">
+      <div class="d-flex flex-row flex-wrap justify-center">
+        <div v-for="number in 10" :key="number">
+          <PostCard :title="title" :description="description" :thumbnail="thumbnail" />
+        </div>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import PostCard from './components/PostCard.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    PostCard,
+  },
+
+  data() {
+    return {
+      title: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus fugiat id nobis, voluptates accusamus dolor maxime nisi, cupiditate iusto rerum repellat minima quis excepturi dignissimos ratione tempora vel ipsam. Rem.',
+      thumbnail: 'https://picsum.photos/1200',
+    };
+  },
+};
+</script>
---
+++ src/components/PostCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card elevation="2" outlined max-width="20rem" max-height="36rem" class="ma-2">
+    <v-img :src="thumbnail" height="250"></v-img>
+    <v-card-title>{{ title }}</v-card-title>
+    <v-card-text>{{ description }}</v-card-text>
+
+    <v-card-actions>
+      <v-btn outlined rounded text>Leia mais</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'PostCard',
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
