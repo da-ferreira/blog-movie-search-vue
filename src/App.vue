8437c41c1c195fa51437afa105d53e0437374699
da-ferreira <ferreiradavid361@gmail.com>
2022-11-28 20:01:25 -0300

    feat: código inicial do vue router

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <template>
-  <v-app>
-    <!-- <Header /> -->
-    <Main />
-  </v-app>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-// import Header from './views/layout/Header.vue';
-import Main from './views/layout/Main.vue';
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: 'App',
+nav {
+  padding: 30px;
 
-  components: {
-    // Header,
-    Main,
-  },
-};
-</script>
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
